bb1ecc2220abea1fb6b043318bb2978eb4f04cf3
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-24 16:15:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 class="text-center text-3xl font-bold text-red-300 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
